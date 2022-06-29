f939cc64400313ebf85b1a6ca464ca8e99887af7
Avijit Hazra <avijithazrabwn@gmail.com>
2022-06-29 13:32:53 +0530

    validationcoompelete

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,9 +1,57 @@
 const collegeModel= require("../models/collegeModels");
 
+ let validUrl =/^(https:\/\/)[a-zA-Z\-_]+\.[a-zA-Z0-9]+\.[a-zA-Z0-9\-_]+\.[a-zA-Z]+\.[a-zA-Z]+\/[a-zA-Z]+\/[a-zA-Z]+\.[a-zA-Z]{2,5}/
+ //let validUrl=require("valid-url")
+
+
+
+const isValid=function(val){
+    if(typeof val==='undefined'|| val ==null) return false
+    //if(typeof val!=='string')return false
+    if(typeof val==='string' && val.trim().length==0)return false
+    return true
+}
+
+
 const createCollege= async function(req,res){
-    let data= req.body;
-    let collegeData= await collegeModel.create(data);
+   
+   try{
+    let {name,fullName,logoLink}=req.body
+       let data= req.body;
+        if(Object.keys(data).length===0){
+            return res.status(400).send({stastus:false,message:"Invalid request parameters plz provide college details"})
+ }
+ if(!isValid(name)){
+    return res.status(400).send({stastus:false,message:"Name is req"})
+}
+if(!isValid(fullName)){
+    return res.status(400).send({stastus:false,message:"fullName is req"})
+}
+
+//for checking duplication
+let uniqueName=await collegeModel.findOne({$or:[{name:name},{fullName:fullName}]})
+if(uniqueName){
+    if(uniqueName.name==name)    return res.status(400).send({stastus:false,message:"Name is already exist"})
+
+    if(uniqueName.fullName==fullName) return res.status(400).send({stastus:false,message:"fullName is already exist"})
+
+}
+
+if(!isValid(logoLink)){
+    return res.status(400).send({stastus:false,message:"logoLink is req"})
+}
+if(!validUrl.test(logoLink)){
+    return res.status(400).send({stastus:false,message:"Enter Valid Url"})
+}
+
+
+
+   let collegeData= await collegeModel.create(data);
     res.status(201).send({status: true, data: collegeData});
+   }catch(err){
+    return res.status(500).send({stastus:false,msg:err.message})
+   }
 }
 
 module.exports.createCollege = createCollege;
+//module.exports={isValid}
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,15 +1,47 @@
 const internModel = require("../models/internModels");
 const collegeModel = require("../models/collegeModels");
+//const validateEmail = require("email-validator")
+const validateEmail=/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+let validMobile = /^[6-9]\d{9}$/  //validating the mobile number (starting from 6 also considerd)
+
+const isValid=function(val){
+    if(typeof val==='undefined'|| val ==null) return false
+    if(typeof val!=='string')return false
+    if(typeof val==='string' && val.trim().length==0)return false
+    return true
+}
 
 const createIntern = async function (req, res) {
     try {
         let userRequest = req.body;
-
+        //check body empty or not
+        if (Object.keys(userRequest).length == 0) {
+            res.status(201).send({ status: false, data: "Keys is req to create Intern" });
+        }
         let { name, email, mobile, collegeName } = userRequest
+        if (!isValid(name)) return res.status(400).send({ status: false, message: "name is req" });
+        if (!isValid(email)) return res.status(400).send({ status: false, message: "email is req" });
+        if (!validMobile.test(mobile)) return res.status(400).send({ status: false, message: "mobile number should contain only numeric numbers and must contain 10 numbers" });
+        if (!isValid(collegeName)) return res.status(400).send({ status: false, message: "collegeName is req" });
 
-        let collegeData = await collegeModel.findOne({ fullName: collegeName }).select({ _id: 1 })
+        //email formate check
+        if (!validateEmail.test(email)) return res.status(400).send({ status: false, message: "email should be in right formate" });
+
+        let uniqueName = await internModel.findOne({ $or: [{ email: email }, { mobile: mobile }] })
+        if (uniqueName) {
+            if (uniqueName.email == email) return res.status(400).send({ stastus: false, message: "email is already exist" })
+            if (uniqueName.mobile == mobile) return res.status(400).send({ stastus: false, message: "mobile is already exist" })
+
+        }
+
+
+        // console.log(collegeName)
+        let collegeData = await collegeModel.findOne({ fullName: collegeName })
+            .select({ _id: 1 })
+        console.log(collegeData)
 
         let collegeId = collegeData._id
+        console.log(collegeId)
 
         let data = { name, email, mobile, collegeId }
 
